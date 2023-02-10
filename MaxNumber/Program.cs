881fefc3f53bcf7239e5f67735c5a980e2853ba1
Aleksey Zagorodnyh <zagor85@bk.ru>
2023-02-10 19:51:01 +0300

    Add MaxNumber

---
+++ MaxNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("Введите первое число : ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число : ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число : ");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+int max = firstNumber;
+
+if (max < secondNumber) 
+{
+max = secondNumber;
+}
+
+if (max < thirdNumber) 
+{
+max = thirdNumber;
+}
+
+Console.WriteLine("Максимальное число : " + max);
